8e0968bea7cb6fb303841dc5a13893afde0f60d4
Wan20 <wandy20number@gmail.com>
2016-04-20 22:52:24 +0700

    LoginActivity Revision and add onclick on activity_login

--- app/src/main/java/com/example/wansu/loginapp/LoginActivity.java
+++ app/src/main/java/com/example/wansu/loginapp/LoginActivity.java
@@ -2,21 +2,24 @@ package com.example.wansu.loginapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView signup_text;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        signup_text = (TextView) findViewById(R.id.sign_up);
-        signup_text.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
-            }
-        });
     }
-}
+
+        public void click(View v)
+        {
+            Intent intent = new Intent(LoginActivity.this,
+                    RegisterActivity.class);
+            startActivity(intent);
+        }
+    }
+
